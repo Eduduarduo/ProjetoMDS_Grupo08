5611fc605030d5055fd73b8802c23d25dc3d734d
lucienemlf <marialucienefelix@gmail.com>
2013-10-26 09:41:49 -0200

    criacao da classe BrasilDao

---
+++ ProjetoMDS_Grupo08/src/br/com/dld/dao/BrasilDao.java
@@ -0,0 +1,51 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.RegiaoMetropolitana;
+
+import Model.Brasil;
+
+import com.mysql.jdbc.ResultSet;
+import com.mysql.jdbc.Statement;
+
+public class BrasilDao extends Dao{
+
+	public BrasilDao() {
+	}
+
+	public List<Brasil> buscaBrasil(int ano){
+
+		List<Brasil> brasil = new ArrayList<Brasil>();
+		
+		try {
+			Connection conexao = this.gerarConexao();
+			String query = "select * from Brasil where ano=" + ano;
+
+			Statement stm = (Statement) conexao.createStatement();
+			ResultSet rs = (ResultSet) stm.executeQuery(query);
+			
+			//varrer toda a tabela e popular o bean
+			while(rs.next()){
+				
+				Brasil br = new Brasil();
+				br.setID(rs.getInt("id"));
+				br.setOpcao(rs.getString("opcao"));
+				br.setValor(rs.getInt("valor"));
+				br.setTipo(rs.getString("tipo"));
+				
+				brasil.add(brasil);	
+			}
+			
+			stm.close();
+			this.fecharConexao(); //fica aki mesmo ou no finally
+
+		} catch (SQLException  sql) {
+			sql.printStackTrace();
+		}
+		return brasil;
+	}
+}
