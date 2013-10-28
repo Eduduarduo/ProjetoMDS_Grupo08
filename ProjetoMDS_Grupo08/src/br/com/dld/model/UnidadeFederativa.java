039fb239e2d991e1729c1c72a8961d612a4d91f3
Vanessa Soares <vannessa.andrade@gmail.com>
2013-10-28 11:10:34 -0300

    Modificação Classe UF

--- ProjetoMDS_Grupo08/src/br/com/dld/model/UnidadeFederativa.java
+++ ProjetoMDS_Grupo08/src/br/com/dld/model/UnidadeFederativa.java
@@ -1,5 +1,14 @@
 package br.com.dld.model;
 
-public class UnidadeFederativa {
+public class UnidadeFederativa extends Dado{
+	//Construtor vazio
+		public UnidadeFederativa(){
+			
+		}
+		
+		//Construtor cheio
+		public UnidadeFederativa(int idUF, int valorUF, String opcaoUF, String tipoUF){
+			super(idUF, valorUF, opcaoUF, tipoUF);
+		}
 
 }
