5c1369587155c67edd2e8502a18e89230f977fb0
Joao-Henrique <joaohenrique.pereira.almeida@gmail.com>
2013-11-13 21:51:22 -0200

    Classe model de Unidade federativa

---
+++ ProjetoMDS_Grupo08/model/UnidadeFederativa.java
@@ -0,0 +1,15 @@
+package model;
+
+public class UnidadeFederativa extends Dado{
+	
+		//Construtor vazio
+		public UnidadeFederativa(){
+			
+		}
+		
+		//Construtor cheio
+		public UnidadeFederativa(String regiaoFederativa, String opcaoFederativa, String tipoFederativo, int anoFederativo, int valorFederativo){
+			super(regiaoFederativa, opcaoFederativa, tipoFederativo, anoFederativo, valorFederativo);
+		}
+
+}
