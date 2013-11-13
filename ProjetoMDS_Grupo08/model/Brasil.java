4642142ab26d9892d3922f6435232180cb014b9b
Joao-Henrique <joaohenrique.pereira.almeida@gmail.com>
2013-11-13 21:48:57 -0200

    classe Brasil

---
+++ ProjetoMDS_Grupo08/model/Brasil.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Brasil extends Dado {
+	 
+	
+	public Brasil() {
+		}
+	
+	public Brasil(String regiaoMetropolitana, String opcaoMetropolitana,String tipoMetropolitano, int anoMetropolitano, int valorMetropolitano){
+		super(regiaoMetropolitana, opcaoMetropolitana, tipoMetropolitano, anoMetropolitano, valorMetropolitano);
+	}
+	
+	}
