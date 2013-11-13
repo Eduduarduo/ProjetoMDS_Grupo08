7e9d995fd090d3698e95ab783afa6370aa0ea253
Joao-Henrique <joaohenrique.pereira.almeida@gmail.com>
2013-11-13 21:45:19 -0200

    Classe de conexao

---
+++ ProjetoMDS_Grupo08/dao/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	
+public Connection getConnection (){
+		
+		
+		try {
+	
+		return DriverManager.getConnection("jdbc:mysql://localhost/dld3","root","root");
+		
+		}
+	 
+	catch (SQLException e) {
+		throw new RuntimeException(e);
+	}
+}
+	
+
+	
+}
