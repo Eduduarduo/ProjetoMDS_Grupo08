e3ecd62c84ccd0e87279d2eb5489085e404ba798
lucienemlf <marialucienefelix@gmail.com>
2013-11-24 00:40:53 -0200

    Criacao do Teste UnidadeFederativaDaoTest

--- ProjetoMDS_Grupo08/test/UnidadeFederativaDaoTest.java
+++ ProjetoMDS_Grupo08/test/UnidadeFederativaDaoTest.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.MockitoAnnotations;
+
 
 import dao.UnidadeFederativaDao;
 
@@ -16,22 +16,39 @@ public class UnidadeFederativaDaoTest {
 	
 	@Before
 	public void init() throws SQLException{
-		MockitoAnnotations.initMocks(this);
-		ufDao = new UnidadeFederativaDao();
-		
-}
-	
-	@Test
-	public void testaQuantidadeReturonoUF() throws SQLException {
-		assertEquals(4, ufDao.buscaUnidadeFederativaAbsoluto(2001, "Acre").size());
-		
+		ufDao = new UnidadeFederativaDao();	
 	}
 	
-	
-	//preciso verificar se a lista está vazia? eu acho desnecessário
 	@Test
-	public void testaSeListaRegiaoMetropolitanaEstaVazia() throws SQLException{
-		assertFalse(ufDao.buscaUnidadeFederativaAbsoluto(2001, "Acre").isEmpty());
+	public void deveRetornarQuantidadeListaUF() throws SQLException {
+		assertEquals(4, ufDao.buscaUnidadeFederativaAbsoluto(2001, "Acre").size());
+		assertNotNull(ufDao.buscaUnidadeFederativaAbsoluto(2001, "Acre"));
+		assertEquals(4, ufDao.buscaUnidadeFederativaRelativo(2001, "Acre").size());
+		assertNotNull(ufDao.buscaUnidadeFederativaRelativo(2001, "Acre"));
+	}
+	
+	@Test
+	public void deveRetornarQuantidadeListaAnos() throws SQLException{
+		assertEquals(9, ufDao.getDatasUnidadeFederativa().size());
+		assertNotNull(ufDao.getDatasUnidadeFederativa());
+	}
+	
+	@Test
+	public void deveRetornarQuantidadeListaAnosComparacao() throws SQLException{
+		assertEquals(8, ufDao.getDatasComparacaoUnidadeFederativa(2009).size());
+		assertNotNull(ufDao.getDatasComparacaoUnidadeFederativa(2009));
+	}
+ 
+	@Test
+	public void deveRetornarQuantidadeListaUFs() throws SQLException{
+		assertEquals(27, ufDao.getUnidadeFederativa().size());
+		assertNotNull(ufDao.getUnidadeFederativa());
+	}
+	
+	@Test
+	public void deveRetornarQuantidadeListaUFsComparacao() throws SQLException{
+		assertEquals(26, ufDao.getUFComparacao("Acre").size());
+		assertNotNull(ufDao.getUFComparacao("Acre"));
 	}
 
 }
