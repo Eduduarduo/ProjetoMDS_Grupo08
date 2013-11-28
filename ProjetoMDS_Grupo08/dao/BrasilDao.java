8561e16373f22fe77cf14bd052220c7161dd0d3c
Joao-Henrique <joaohenrique.pereira.almeida@gmail.com>
2013-11-28 20:56:07 -0200

    refatoração da classe brasilDao

--- ProjetoMDS_Grupo08/dao/BrasilDao.java
+++ ProjetoMDS_Grupo08/dao/BrasilDao.java
@@ -6,122 +6,103 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import exception.ExceptionsBrasil;
+
 import model.Brasil;
 
 public class BrasilDao extends ConnectionFactory {
 
-	// Variavel de conexao usada para todos os metodos da classe
 	private Connection conexao;
+	public ExceptionsBrasil exception = new ExceptionsBrasil();
 
-	// Contrutor da classe
 	public BrasilDao() {
 	}
 
-	// Metodo que busca todos os valores Absolutos de Brasil
 	public List<Brasil> buscaBrasilAbsoluto(int ano) throws SQLException {
-		//Instancia que será de retorno
 		List<Brasil> brasil = new ArrayList<Brasil>();
-		// Criando uma conexao com a Classe ConnectionFactory
+		if (exception.verificaParamentroAno(ano) == true) {
+			this.conexao = new ConnectionFactory().getConnection();
+			String query = "select * from dldbrasilabsoluto where ano = ? ";
+			java.sql.PreparedStatement stm = this.conexao
+					.prepareStatement(query);
+			stm.setInt(1, ano);
+			ResultSet rs = stm.executeQuery();
+			while (rs.next()) {
+				Brasil br = new Brasil();
+				br.setRegiao(rs.getString("regiao"));
+				br.setOpcao(rs.getString("opcao"));
+				br.setTipo("Absoluto");
+				br.setAno(rs.getInt("ano"));
+				br.setValor(rs.getInt("valor"));
+				brasil.add(br);
+			}
+			stm.close();
+			conexao.close();
+			return brasil;
+		}else
+		return brasil;
+	}
+
+	public List<Brasil> buscaBrasilRelativo(int ano) throws SQLException {
+		List<Brasil> brasil = new ArrayList<Brasil>();
+		if (exception.verificaParamentroAno(ano) == true) {
+			this.conexao = new ConnectionFactory().getConnection();
+			String query = "select * from dldbrasilrelativo where ano = ? ";
+			java.sql.PreparedStatement stm = this.conexao
+					.prepareStatement(query);
+			stm.setInt(1, ano);
+			ResultSet rs = stm.executeQuery();
+			while (rs.next()) {
+				Brasil br = new Brasil();
+				br.setRegiao(rs.getString("regiao"));
+				br.setOpcao(rs.getString("opcao"));
+				br.setTipo("Relativo");
+				br.setAno(rs.getInt("ano"));
+				br.setValor(rs.getInt("valor"));
+				brasil.add(br);
+			}
+			conexao.close();
+			stm.close();
+		}
+		return brasil;
+	}
+
+	public List<Brasil> getDatas() throws SQLException {
+		List<Brasil> datas = new ArrayList<Brasil>();
 		this.conexao = new ConnectionFactory().getConnection();
-		//Comando query 
-		String query = "select * from dldbrasilabsoluto where ano = ? ";
+		String query = "select distinct ano from dldbrasilabsoluto "
+				+ "UNION select distinct ano from dldbrasilrelativo";
 		java.sql.PreparedStatement stm = this.conexao.prepareStatement(query);
-		//Parametro de busca 
-		stm.setInt(1, ano);
 		ResultSet rs = stm.executeQuery();
-
 		while (rs.next()) {
-			//Criando as classe para criação da lista
-			Brasil br = new Brasil();
-			br.setRegiao(rs.getString("regiao"));
-			br.setOpcao(rs.getString("opcao"));
-			br.setTipo("Absoluto");
-			br.setAno(rs.getInt("ano"));
-			br.setValor(rs.getInt("valor"));
-			brasil.add(br);
-
+			Brasil dt = new Brasil();
+			dt.setAno(rs.getInt("ano"));
+			datas.add(dt);
 		}
 		stm.close();
 		conexao.close();
-		//retorno da lista
-		return brasil;
+		return datas;
 	}
-	
-	public List<Brasil> buscaBrasilRelativo(int ano) throws SQLException {
-		
-		List<Brasil> brasil = new ArrayList<Brasil>();
-		// Criando uma conexao com a Classe ConnectionFactory
-		this.conexao = new ConnectionFactory().getConnection();
-		String query = "select * from dldbrasilrelativo where ano = ? ";
-		java.sql.PreparedStatement stm = this.conexao.prepareStatement(query);
-		stm.setInt(1, ano);
-		ResultSet rs = stm.executeQuery();
 
-		while (rs.next()) {
-
-			Brasil br = new Brasil();
-			br.setRegiao(rs.getString("regiao"));
-			br.setOpcao(rs.getString("opcao"));
-			br.setTipo("Relativo");
-			br.setAno(rs.getInt("ano"));
-			br.setValor(rs.getInt("valor"));
-			brasil.add(br);
-
+	public List<Brasil> getDatasComparação(int ano) throws SQLException {
+		List<Brasil> datas = new ArrayList<Brasil>();
+		if (exception.verificaParamentroAno(ano) == true) {
+			this.conexao = new ConnectionFactory().getConnection();
+			String query = "select distinct ano from dldbrasilabsoluto where ano !=? "
+					+ "UNION select distinct ano from dldbrasilrelativo where ano !=?";
+			java.sql.PreparedStatement stm = this.conexao
+					.prepareStatement(query);
+			stm.setInt(1, ano);
+			stm.setInt(2, ano);
+			ResultSet rs = stm.executeQuery();
+			while (rs.next()) {
+				Brasil dt = new Brasil();
+				dt.setAno(rs.getInt("ano"));
+				datas.add(dt);
+			}
+			stm.close();
+			conexao.close();
 		}
-		stm.close();
-		return brasil;
+		return datas;
 	}
-	
-public List<Integer>  getDatas() throws SQLException{
-	
-	List<Integer> datas = new ArrayList<Integer>();
-	this.conexao = new ConnectionFactory().getConnection();
-	//
-	String query ="select distinct ano from dldbrasilabsoluto " +
-			"UNION select distinct ano from dldbrasilrelativo";
-	java.sql.PreparedStatement stm = this.conexao.prepareStatement(query);
-	ResultSet rs = stm.executeQuery();
-
-	while (rs.next()) {
-		int dt;
-		dt = rs.getInt("ano");
-
-		datas.add(dt);
-	}
-	stm.close();
-	conexao.close();
-	return datas;
-	
-}	
-
-public List<Integer>  getDatasComparacao(int ano) throws SQLException{
-	
-	List<Integer> datas = new ArrayList<Integer>();
-	this.conexao = new ConnectionFactory().getConnection();
-	//Comando select faz uma busca e filtra as datas e exclui 
-	//a data que ja foi pesquisada, as datas são comuns em ambas tabelas,
-	//filtra tbm os dados repetidos
-	String query ="select distinct ano from dldbrasilabsoluto where ano !=? " +
-			"UNION select distinct ano from dldbrasilrelativo where ano !=?";
-	java.sql.PreparedStatement stm = this.conexao.prepareStatement(query);
-	stm.setInt(1,ano);
-	stm.setInt(2,ano);
-	
-	ResultSet rs = stm.executeQuery();
-
-	while (rs.next()) {
-		int dt;
-		dt = rs.getInt("ano");
-
-		datas.add(dt);
-	}
-	stm.close();
-	conexao.close();
-	return datas;
-	
-}	
-
-
-
-
 }
