357d083f827221d04536ef3b9f84276666bdf08f
Joao-Henrique <joaohenrique.pereira.almeida@gmail.com>
2013-11-28 20:56:55 -0200

    Refatoração de UfDao

--- ProjetoMDS_Grupo08/dao/UnidadeFederativaDao.java
+++ ProjetoMDS_Grupo08/dao/UnidadeFederativaDao.java
@@ -5,161 +5,150 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
+import exception.ExceptionsUF;
 
 import model.UnidadeFederativa;
 
 public class UnidadeFederativaDao {
 
-	private Connection conexao;
+	public Connection conexao;
+	public ExceptionsUF exception = new ExceptionsUF();
 
-		public UnidadeFederativaDao() {
-		}
-	
-	public List<UnidadeFederativa> buscaUnidadeFederativaAbsoluto(int ano,String UF) throws SQLException {
-	
-			List<UnidadeFederativa> unidadeFederativa = new ArrayList<UnidadeFederativa>();
-			this.conexao = new ConnectionFactory().getConnection();
-			String query = "select * from dldlufoabsoluto where ano = ? and regiao = ? ";
-			java.sql.PreparedStatement stm = this.conexao.prepareStatement(query);
-			stm.setInt(1, ano);
-			stm.setString(2,UF);
-			ResultSet rs = stm.executeQuery();
+	public UnidadeFederativaDao() {
+	}
 
-			while (rs.next()) {
-			
-				UnidadeFederativa uf = new UnidadeFederativa();
-				uf.setRegiao(rs.getString("regiao"));
-				uf.setOpcao(rs.getString("opcao"));
-				uf.setTipo("Absoluto");
-				uf.setAno(rs.getInt("ano"));
-				uf.setValor(rs.getInt("valor"));
-				unidadeFederativa.add(uf);
-
-			}
-			stm.close();
-			conexao.close();
-			return unidadeFederativa;
-		}
-		
-	public List<UnidadeFederativa> buscaUnidadeFederativaRelativo(int ano,String UF) throws SQLException {
-			
-			List<UnidadeFederativa> unidadeFederativa = new ArrayList<UnidadeFederativa>();
-			this.conexao = new ConnectionFactory().getConnection();
-			String query = "select * from dldufrelativo where ano = ? and regiao = ? ";
-			java.sql.PreparedStatement stm = this.conexao.prepareStatement(query);
-			stm.setInt(1, ano);
-			stm.setString(2,UF);
-			ResultSet rs = stm.executeQuery();
-
-			while (rs.next()) {
-
-				UnidadeFederativa uf = new UnidadeFederativa();
-				uf.setRegiao(rs.getString("regiao"));
-				uf.setOpcao(rs.getString("opcao"));
-				uf.setTipo("Relativo");
-				uf.setAno(rs.getInt("ano"));
-				uf.setValor(rs.getInt("valor"));
-				unidadeFederativa.add(uf);
-
-			}
-			stm.close();
-			conexao.close();
-			return unidadeFederativa;
-		}
-		
-	public List<Integer>  getDatasUnidadeFederativa() throws SQLException{
-		
-		List<Integer> datasUF = new ArrayList<Integer>();
+	public List<UnidadeFederativa> buscaUnidadeFederativaAbsoluto(int ano,
+			String ufParametro) throws SQLException {
 		this.conexao = new ConnectionFactory().getConnection();
-		String query ="select distinct ano from dldufbsoluto UNION select distinct ano from dldufrelativo";
+
+		List<UnidadeFederativa> unidadeFederativaList = new ArrayList<UnidadeFederativa>();
+
+		if (exception.verificaParamentroUF(ufParametro) == true) {
+		
+				String query = "select * from dldufabsoluto where ano =? and regiao =? ";
+				java.sql.PreparedStatement stm = this.conexao
+						.prepareStatement(query);
+				stm.setInt(1, ano);
+				stm.setString(2, ufParametro);
+				ResultSet rs = stm.executeQuery();
+				while (rs.next()) {
+					UnidadeFederativa uf = new UnidadeFederativa();
+					uf.setRegiao(rs.getString("regiao"));
+					uf.setOpcao(rs.getString("opcao"));
+					uf.setTipo(" Absoluto");
+					uf.setAno(rs.getInt("ano"));
+					uf.setValor(rs.getInt("valor"));
+					unidadeFederativaList.add(uf);
+				}
+				stm.close();
+				conexao.close();
+				return unidadeFederativaList;
+			} else {
+				return unidadeFederativaList;
+			}
+		
+	}
+
+	public List<UnidadeFederativa> buscaUnidadeFederativaRelativo(int ano,
+			String ufParametro) throws SQLException {
+
+		List<UnidadeFederativa> unidadeFederativaList = new ArrayList<UnidadeFederativa>();
+		if (exception.verificaParamentroUF(ufParametro) == true) {
+				this.conexao = new ConnectionFactory().getConnection();
+				String query = "select * from dldufrelativo where ano = ? and regiao = ? ";
+				java.sql.PreparedStatement stm = this.conexao
+						.prepareStatement(query);
+				stm.setInt(1, ano);
+				stm.setString(2, ufParametro);
+				ResultSet rs = stm.executeQuery();
+				while (rs.next()) {
+					UnidadeFederativa uf = new UnidadeFederativa();
+					uf.setRegiao(rs.getString("regiao"));
+					uf.setOpcao(rs.getString("opcao"));
+					uf.setTipo("Relativo");
+					uf.setAno(rs.getInt("ano"));
+					uf.setValor(rs.getInt("valor"));
+					unidadeFederativaList.add(uf);
+				}
+				stm.close();
+				conexao.close();
+				return unidadeFederativaList;
+			}else return unidadeFederativaList;
+		}
+
+	public List<UnidadeFederativa> getAnosUnidadeFederativa()
+			throws SQLException {
+
+		List<UnidadeFederativa> datasUF = new ArrayList<UnidadeFederativa>();
+		this.conexao = new ConnectionFactory().getConnection();
+		String query = "select  ano from dldufabsoluto UNION select  ano from dldufrelativo";
 		java.sql.PreparedStatement stm = this.conexao.prepareStatement(query);
 		ResultSet rs = stm.executeQuery();
-
 		while (rs.next()) {
-			int dataUF;
-			dataUF = rs.getInt("ano");
-
+			UnidadeFederativa dataUF = new UnidadeFederativa();
+			dataUF.setRegiao(rs.getString("ano"));
 			datasUF.add(dataUF);
 		}
 		stm.close();
 		conexao.close();
 		return datasUF;
-		
-	}	
-	 
-	public List<String>  getUnidadeFederativa() throws SQLException{
-		
-		List<String> datasUF = new ArrayList<String>();
+	}
+
+	public List<UnidadeFederativa> getUnidadeFederativa() throws SQLException {
+
+		List<UnidadeFederativa> datasUF = new ArrayList<UnidadeFederativa>();
 		this.conexao = new ConnectionFactory().getConnection();
-		String query ="select distinct regiao from dldufbsoluto UNION select distinct regiao from dldufrelativo";
+		String query = "select distinct regiao from dldufabsoluto UNION select distinct regiao from dldufrelativo";
 		java.sql.PreparedStatement stm = this.conexao.prepareStatement(query);
 		ResultSet rs = stm.executeQuery();
-
 		while (rs.next()) {
-			String dataUF;
-			dataUF = rs.getString("regiao");
-
+			UnidadeFederativa dataUF = new UnidadeFederativa();
+			dataUF.setRegiao(rs.getString("regiao"));
 			datasUF.add(dataUF);
 		}
 		stm.close();
 		conexao.close();
 		return datasUF;
-		
-	}	
+	}
 
-	
-	
-	
-	public List<Integer>  getDatasComparacaoUnidadeFederativa(int ano) throws SQLException{
-		
-		List<Integer> datasUF = new ArrayList<Integer>();
-		this.conexao = new ConnectionFactory().getConnection();
-		String query ="select distinct ano from dldufabsoluto where ano !=? " +
-				"UNION select distinct ano from dldufrelativo where ano !=?";
-		java.sql.PreparedStatement stm = this.conexao.prepareStatement(query);
-		stm.setInt(1,ano);
-		stm.setInt(2,ano);
-		
-		ResultSet rs = stm.executeQuery();
+	public List<UnidadeFederativa> getAnosComparaçãoUnidadeFederativa(int ano)
+			throws SQLException {
+		List<UnidadeFederativa> anosUF = new ArrayList<UnidadeFederativa>();
 
-		while (rs.next()) {
-			int dtUF;
-			dtUF = rs.getInt("ano");
+			this.conexao = new ConnectionFactory().getConnection();
+			String query = "select  ano from dldufabsoluto where ano != ? UNION select  ano from dldufrelativo where ano != ? ";
+			java.sql.PreparedStatement stm = this.conexao.prepareStatement(query);
+			stm.setInt(1,ano);
+			stm.setInt(2,ano);
+			ResultSet rs = stm.executeQuery();
+			while (rs.next()) {
+				UnidadeFederativa dtUF = new UnidadeFederativa();
+				dtUF.setAno(rs.getInt("ano"));
+				anosUF.add(dtUF);
+			}
+			stm.close();
+			conexao.close();
+			return anosUF;
+	}
 
-			datasUF.add(dtUF);
+	public List<UnidadeFederativa> getUFComparacao(String regiao) throws SQLException {
+
+		List<UnidadeFederativa> datasUF = new ArrayList<UnidadeFederativa>();
+		if ((exception.verificaParamentroUF(regiao)) == true) {
+			conexao = new ConnectionFactory().getConnection();
+			String query = "select distinct regiao from dldufabsoluto where regiao !=? UNION select distinct ano from dldufrelativo where regiao != ? ;";
+			java.sql.PreparedStatement stm = conexao.prepareStatement(query);
+			stm.setString(1,regiao);
+			stm.setString(2,regiao);
+			ResultSet rs = stm.executeQuery();
+			while (rs.next()) {
+				UnidadeFederativa dtUF = new UnidadeFederativa();
+				dtUF.setRegiao(rs.getString("regiao"));
+				datasUF.add(dtUF);
+			}
+			stm.close();
+			conexao.close();
 		}
-		stm.close();
-		conexao.close();
 		return datasUF;
-		
-	}	
-
-public List<String>  getUFComparacao(String regiao) throws SQLException{
-		
-		List<String> datasUF = new ArrayList<String>();
-		this.conexao = new ConnectionFactory().getConnection();
-		String query ="select distinct ano from dldufabsoluto where regiao !=? " +
-				"UNION select distinct ano from dldufrelativo where regiao !=?";
-		java.sql.PreparedStatement stm = this.conexao.prepareStatement(query);
-		stm.setString(1,regiao);
-		stm.setString(2,regiao);
-		
-		ResultSet rs = stm.executeQuery();
-
-		while (rs.next()) {
-			String dtUF;
-			dtUF = rs.getString("regiao");
-
-			datasUF.add(dtUF);
-		}
-		stm.close();
-		conexao.close();
-		return datasUF;
-		
-	}	
-
-	
-	
-	
+	}
 }
