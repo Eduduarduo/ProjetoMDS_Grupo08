0dff9bdd6b549052123a2640b96dc7bf495482d2
lucienemlf <marialucienefelix@gmail.com>
2013-11-29 17:28:19 -0200

    refatoracao da classe ExceptionRegiaoMetropolitana

--- ProjetoMDS_Grupo08/exception/ExceptionRegiaoMetropolitana.java
+++ ProjetoMDS_Grupo08/exception/ExceptionRegiaoMetropolitana.java
@@ -2,101 +2,123 @@ package exception;
 
 import java.sql.SQLException;
 import java.util.List;
+
+import dao.RegiaoMetropolitanaDao;
+import dao.UnidadeFederativaDao;
 import model.RegiaoMetropolitana;
+import model.UnidadeFederativa;
 
 public class ExceptionRegiaoMetropolitana {
+	
+	private RegiaoMetropolitanaDao  regiaoDao;
 
 	public boolean verificaCampoDeParametro(int ano) throws SQLException {
+		regiaoDao = new RegiaoMetropolitanaDao();
+		
+		for(int i = 0; i < regiaoDao.getAnosRegiaoMetropolitana().size(); i++)
+			if(regiaoDao.getAnosRegiaoMetropolitana().get(i).getAno() == ano)
+				return true;
 
-		boolean validacao = true;
-
-		if (ano == 0) {
-			validacao = false;
-		}
-
-		return validacao;
+		return false;
 	}
 
 	public boolean verificaCampoDeParametro(String regiao) throws SQLException {
+		regiaoDao = new RegiaoMetropolitanaDao();
+		
+		for(int i = 0; i < regiaoDao.getRegioesRegiaoMetropolitana().size(); i++)
+			if(regiaoDao.getRegioesRegiaoMetropolitana().get(i).getRegiao().equals(regiao))
+				return true;
 
-		boolean validacao = true;
-
-		if (regiao == null) {
-			validacao = false;
-		}
-
-		return validacao;
+		return false;
 	}
 
 	public boolean verificaCampoDeParametro(String regiao, int ano)
 			throws SQLException {
 
-		boolean validacao = true;
-
-		if (ano == 0 || regiao == null) {
-			validacao = false;
-		}
-
-		return validacao;
+		if(verificaCampoDeParametro(ano) == false)
+			return false;
+		if(verificaCampoDeParametro(regiao) == false)
+			return false;
+		
+		return true;
 	}
 
 	public boolean verificaCampoDeParametro(String regiao, int ano, int ano2)
 			throws SQLException {
-
-		boolean validacao = true;
-
-		if (ano == 0 || regiao == null || ano2 == 0) {
-			validacao = false;
-		}
-
-		return validacao;
+		if(ano == ano2)
+			return false;
+		if(verificaCampoDeParametro(ano) == false)
+			return false;
+		if(verificaCampoDeParametro(ano2) == false)
+			return false;
+		if(verificaCampoDeParametro(regiao) == false)
+			return false;
+		
+		return true;	
 	}
 
 	public boolean verificaCampoDeParametro(String regiao, String regiao2,
 			int ano) throws SQLException {
-
-		boolean validacao = true;
-
-		if (ano == 0 || regiao == null || regiao2 == null) {
-			validacao = false;
-		}
-
-		return validacao;
+		if(regiao == regiao2)
+			return false;
+		if(verificaCampoDeParametro(ano) == false)
+			return false;
+		if(verificaCampoDeParametro(regiao2) == false)
+			return false;
+		if(verificaCampoDeParametro(regiao) == false)
+			return false;
+		
+		return true;	
 	}
 
-	public boolean validaListBox(List<String> listaRegiao) {
+	public boolean validaListBox(List<RegiaoMetropolitana> listaRegiao) {
+		RegiaoMetropolitana regiao = new RegiaoMetropolitana();
 		if (listaRegiao.isEmpty() == true)
 			return false;
-		else
+		if(listaRegiao.equals(regiao))
 			return true;
+		
+		return false;
 	}
 
-	public boolean validaAnosListBox(List<Integer> anos) {
-		if (anos.isEmpty() == true)
+	public boolean validaListBoxAbsoluto(List<RegiaoMetropolitana> listaRegiao) {
+
+		if (validaListBox(listaRegiao) == false)
 			return false;
-		else
-			return true;
+		if (listaRegiao.get(1).getTipo() != "Absoluto")
+			return false;
+		return true;
 	}
 
-	public boolean validaLista(List<RegiaoMetropolitana> listaData) {
-		boolean validacao = true;
+	public boolean validaListBoxRelativo(List<RegiaoMetropolitana> listaRegiao) {
 
-		if (listaData.isEmpty() == true) {
-			validacao = false;
+		if (validaListBox(listaRegiao) == false)
+			return false;
+		if (listaRegiao.get(1).getTipo() != "Relativo")
+			return false;
+		return true;
+	}
+
+	//usado no metodo Busca
+	public boolean validaGerarGraficoBusca(List<RegiaoMetropolitana> listaRegiaoAbsoluto,
+			List<RegiaoMetropolitana> listaRegiaoRelativo) {
+
+		if (validaListBoxAbsoluto(listaRegiaoAbsoluto) == false &&
+				validaListBoxRelativo(listaRegiaoRelativo) == false) {
+			return false;
 		}
-		return validacao;
+		return true;
 	}
-
-	public boolean validaListasGraficos(List<RegiaoMetropolitana> listaData1,
-			List<RegiaoMetropolitana> listaData2) {
 	
-		boolean validacao = true;
-		boolean val = validaLista(listaData1);
-		boolean val2 = validaLista(listaData2);
+	//usado no metodo comparacao
+	public boolean validaGerarGraficoComparacao(List<RegiaoMetropolitana> listaRegiaoAbsoluto,
+			List<RegiaoMetropolitana> listaRegiaoAbsoluto2) {
 
-		if (val == false || val2 == false) {
-			validacao = false;
+		if (validaListBoxAbsoluto(listaRegiaoAbsoluto) == false && validaListBoxAbsoluto(listaRegiaoAbsoluto2) == false) {
+			return false;
 		}
-		return validacao;
+		return true;
 	}
+	
+	
 }
