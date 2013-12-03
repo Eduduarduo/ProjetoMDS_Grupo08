ed547e9d9933373cb4d8f1c50d42d17497efe87c
lucienemlf <marialucienefelix@gmail.com>
2013-12-03 09:05:12 -0200

    arrumando os testes

--- ProjetoMDS_Grupo08/test/RegiaoMetropolitanaDaoTest.java
+++ ProjetoMDS_Grupo08/test/RegiaoMetropolitanaDaoTest.java
@@ -52,11 +52,15 @@ public class RegiaoMetropolitanaDaoTest {
 		assertNotNull(regiaoDao.getRegioesRegiaoMetropolitana());
 	}
 	
+	@Test
+	public void deveRetornarQuantidadeListaRegioesComparacao() throws SQLException{
+		assertEquals(9, regiaoDao.getRegioesComparacaoRegiaoMetropolitana("Belem - PA").size());
+		assertNotNull(regiaoDao.getRegioesComparacaoRegiaoMetropolitana("Belem - PA"));
+	}
 
 	
 	@Test
 	public void testeException() throws SQLException{
-		//expect(this.exception.verificaCampoDeParametro("Belem - PA", 2001)).andReturn(true);
 		assertTrue(this.exception.verificaCampoDeParametro("Belem - PA", 2001));
 	}
 	
